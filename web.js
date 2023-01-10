c317867ec00ffde81c02795493328559e8eb78c0
Kalpesh Mahale <67682474+Kalpesh1012@users.noreply.github.com>
2023-01-10 14:37:33 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Heading 1"
+);
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "Heading 2"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
